ce3fb324793a4b1d6a9acc305523f58b0813c51a
amelija04 <63237336+amelija04@users.noreply.github.com>
2021-01-31 23:29:20 +0100

    Potrebno je da se popune sva polja

--- hw-4/login.php
+++ hw-4/login.php
@@ -3,15 +3,20 @@
     <head>
     
         <link rel="stylesheet" href="style.css">
-
-        <body>
-       
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script>
+             function signUp(){
+                   window.location.href="signUp.php";
+             }
+        </script> 
+        <style>
+            body{
+                background-image:url('pozadina.jpg');
+            }
+         </style>
+        <body>       
             <?php
-             
-
             $username = $password ="";
-
-
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $username = test_input($_POST["username"]);
                 $passsword = test_input($_POST["password"]);
@@ -25,15 +30,15 @@
             ?>
             <div class="central">
                 
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-                Username: <br> <input type="text" name="username">       
+            <form method="post" action="addition.php">  
+                <input type="text" name="username" placeholder="Username..">       
                 <br><br>
-                Password: <br> <input type="text" name="password">  
+                <input type="text" name="password" placeholder="Password..">  
                 <br><br>
                 <input  class="btn" type="submit" name="log in" value="Log in">  
                 </form>
                 <p>Don't have an account? Create one:</p>
-                <input  class="btn" type="submit" name="sign up" value="Sign up">
+                <input  class="btn" onclick="signUp()" type="submit" name="sign up" value="Sign up">
         </div>
         </body>
     </head>
