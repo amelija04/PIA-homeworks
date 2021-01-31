218fc3f7bd4c2935cdabb524880a2d874819e310
amelija04 <63237336+amelija04@users.noreply.github.com>
2021-01-31 23:23:36 +0100

    Potrebno je da se popune sva polja

--- hw-4/database.php
+++ hw-4/database.php
@@ -10,9 +10,25 @@ $surname=$_POST['surname'];
 $email=$_POST['email'];
 $username=$_POST['username'];
 $password=$_POST['password'];
-
+$type="regular";
  
 $sql="insert into korisnik (name, surname, email, username, password) values ('$name', '$surname', '$email', '$username', '$password')";
 $query=mysqli_query($con, $sql);
+
+function emptyInput($name, $surname, $email, $username, $password){
+    if(empty($name) || empty($surname) || empty($email) || empty($username) || empty($password) ){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+if(emptyInput($name, $surname, $email, $username, $password)==true){
+    header("Location: signUp.php");
+    exit();
+}
+else{
+    header("Location: main.php");    
+}
 
 ?>
